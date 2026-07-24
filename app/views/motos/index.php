7fa2bcaa1056c23f07e2d5828f633c48f4630795
rrene7 <rrenys.evang0907@gmail.com>
2026-07-24 11:35:49 -0500

    Agregar miniaturas al listado de motocicletas

--- app/views/motos/index.php
+++ app/views/motos/index.php
@@ -8,20 +8,27 @@
 </form>
 <div class="table-wrap panel">
 <table>
-    <thead><tr><th>Código</th><th>Motocicleta</th><th>Placa</th><th>Unidad</th><th>Kilometraje</th><th>Estado</th><th></th></tr></thead>
+    <thead><tr><th>Foto</th><th>Código</th><th>Motocicleta</th><th>Placa</th><th>Unidad</th><th>Kilometraje</th><th>Estado</th><th></th></tr></thead>
     <tbody>
     <?php foreach ($motos as $moto): ?>
         <tr>
+            <td>
+                <?php if (!empty($moto['foto'])): ?>
+                    <img class="moto-thumb" src="<?= e(base_url($moto['foto'])) ?>" alt="">
+                <?php else: ?>
+                    <span class="moto-thumb-empty" aria-label="Sin fotografía">🏍️</span>
+                <?php endif; ?>
+            </td>
             <td><strong><?= e($moto['codigo_qr']) ?></strong></td>
             <td><?= e($moto['marca'] . ' ' . $moto['modelo']) ?><br><small>Año <?= e((string)$moto['anio']) ?></small></td>
             <td><?= e($moto['placa']) ?></td>
             <td><?= e($moto['unidad_asignada']) ?></td>
             <td><?= number_format((int)$moto['kilometraje_actual']) ?> km</td>
             <td><span class="badge <?= e(status_badge($moto['estado'])) ?>"><?= e($moto['estado']) ?></span></td>
-            <td class="actions"><a class="btn btn-small" href="<?= e(base_url('motos/ver?id=' . $moto['id'])) ?>">Ver</a></td>
+            <td class="actions"><a class="btn btn-small" href="<?= e(base_url('motos/ver?id=' . $moto['id'])) ?>">Ver expediente</a></td>
         </tr>
     <?php endforeach; ?>
-    <?php if (!$motos): ?><tr><td colspan="7" class="empty">No se encontraron motocicletas.</td></tr><?php endif; ?>
+    <?php if (!$motos): ?><tr><td colspan="8" class="empty">No se encontraron motocicletas.</td></tr><?php endif; ?>
     </tbody>
 </table>
 </div>
