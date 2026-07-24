2b93ad2950e568342bad70085b32be28b3db4d45
rrene7 <rrenys.evang0907@gmail.com>
2026-07-24 10:47:51 -0500

    Agregar ejemplo de configuración local

---
+++ app/config/config.local.example.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'db_host' => '127.0.0.1',
+    'db_port' => '3306',
+    'db_name' => 'lince_motos',
+    'db_user' => 'root',
+    'db_pass' => '',
+];
